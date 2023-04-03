c7ea6a242016fb1e5d85b9e5b0e689e335d2b9df
kuangzhongcan <xyl8223004369>
2023-04-03 08:12:45 +0800

    用户中心

---
+++ ACGBox-User/ACGBox-User-Core/src/main/java/wiki/acgcsbox/Application.java
@@ -0,0 +1,15 @@
+package wiki.acgcsbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaobai
+ * @version 1.0
+ * @description 用户模块
+ * @date 2023-4-2 17:54:01
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+}
