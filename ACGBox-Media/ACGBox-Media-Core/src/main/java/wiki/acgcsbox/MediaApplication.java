2a0d402343a5ae1e590b1f1b75116d57d605b63d
Krian <2793260947@qq.com>
2023-04-05 13:40:37 +0800

    ✏合并Master分支提交！ 2023-04-05

---
+++ ACGBox-Media/ACGBox-Media-Core/src/main/java/wiki/acgcsbox/MediaApplication.java
@@ -0,0 +1,17 @@
+package wiki.acgcsbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Krian
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/4/4 0004 18:47
+ */
+@SpringBootApplication
+public class MediaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaApplication.class, args);
+    }
+}
---
+++ ACGBox-Media/ACGBox-Media-Core/src/main/java/wiki/acgcsbox/controller/ImageshackController.java
@@ -0,0 +1,16 @@
+package wiki.acgcsbox.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Krian
+ * @version 1.0
+ * @description: TODO 图床接口，上传图片，下载图片，随机返回图片，返回分类图片...
+ * @date 2023/4/4 0004 18:52
+ */
+@RestController
+@RequestMapping("/imageshack")
+public class ImageshackController {
+
+}
